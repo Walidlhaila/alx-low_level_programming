93ae5d0aa3cc2899746e7aa74e412597242286d8
Walidlhaila <walidlhaila96@gmail.com>
2023-06-21 15:06:07 -0700

    102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,32 +1,15 @@
 #include <stdio.h>
 
-/**
- * main - finds and prints the sum of the even-valued terms
- * followed by a new line
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-	int i;
-	unsigned long int j, k, next, sum;
+import sys
 
+def fibonacci_sequence(n):
+    sequence = [1, 2]  # Starting with 1 and 2
+    for i in range(2, n):
+        sequence.append(sequence[i-1] + sequence[i-2])
+    return sequence
 
+fib_numbers = fibonacci_sequence(50)
+fib_str = ', '.join(map(str, fib_numbers))
+print(fib_str)
+sys.stdout.flush()  # Ensure the output is flushed immediately
 
-	j = 1;
-	k = 2;
-	sum = 0;
-
-	for (i = 1; i <= 33; ++i)
-	{
-		if (j < 4000000 && (j % 2) == 0)
-		{
-			sum = sum + j;
-		}
-		next = j + k;
-		j = k;
-		k = next;
-	}
-
-	printf("%lu\n", sum);
-	return (0);
-}
