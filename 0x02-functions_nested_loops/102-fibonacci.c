27b9164b65e0805001f385f21da36af1520508d6
Walidlhaila <walidlhaila96@gmail.com>
2023-06-21 15:15:15 -0700

    102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,27 +1,31 @@
 #include <stdio.h>
 
-void fibonacci_sequence(int n) {
-    int i;
-    unsigned long long fib[50];  // Use unsigned long long to handle large Fibonacci numbers
-    
-    fib[0] = 1;
-    fib[1] = 2;
-    
-    for (i = 2; i < n; i++) {
-        fib[i] = fib[i-1] + fib[i-2];
-    }
-    
-    for (i = 0; i < n; i++) {
-        printf("%llu", fib[i]);
-        if (i != n-1) {
-            printf(", ");
-        }
-    }
-    
-    printf("\n");
+void fibonacci_sequence(int n)
+{
+	int i;
+	unsigned long fib[50];  // Use unsigned long to handle large Fibonacci numbers
+
+	fib[0] = 1;
+	fib[1] = 2;
+
+	for (i = 2; i < n; i++)
+		fib[i] = fib[i-1] + fib[i-2];
 }
 
-int main() {
-    fibonacci_sequence(50);
-    return 0;
+for (i = 0; i < n; i++)
+{
+	printf("%lu", fib[i]);
+	if (i != n-1)
+	{
+		printf(", ");
+	}
+}
+
+printf("\n");
+}
+
+int main()
+{
+	fibonacci_sequence(50);
+	return 0;
 }
