f1fa0039630ebdc02da113eb228dbce5ea8dcbb7
Walidlhaila <walidlhaila96@gmail.com>
2023-06-22 14:14:53 -0700

    2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,21 +1,10 @@
-#include "main.h"
 #include <stdio.h>
 
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
+int mul(int a, int b);
 
 int main(void)
-
 {
-
-    char c;
-
-    c = '0';
-    printf("%c: %d\n", c, _isdigit(c));
-    c = 'a';
-    printf("%c: %d\n", c, _isdigit(c));
-    return (0);
+    printf("%d\n", mul(98, 1024));
+    printf("%d\n", mul(-402, 4096));
+    return 0;
 }
