a749447d5daaa00dc03d55f90732b64c705157e9
Walidlhaila <walidlhaila96@gmail.com>
2023-07-17 17:43:24 -0700

    5-free_dog.c

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -9,11 +9,9 @@ void free_dog(dog_t *d)
 {
     if (d == NULL)
         return;
-
-    /* Free the memory for the name and owner strings */
+{
     free(d->name);
     free(d->owner);
-
-    /* Free the memory for the dog struct */
     free(d);
+}
 }
