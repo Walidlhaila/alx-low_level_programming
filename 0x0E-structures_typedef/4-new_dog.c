55c1fe3202a1631544c9c079958c47cba04a58a6
Walidlhaila <walidlhaila96@gmail.com>
2023-07-17 17:36:54 -0700

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,54 +1,22 @@
 #include <stdlib.h>
-#include <string.h>
 #include "dog.h"
 
 /**
- * new_dog - creates a new dog.
- * @name: the name of the dog.
- * @age: the age of the dog.
- * @owner: the owner of the dog.
+ * _strlen - returns the length of a string
+ * @s: string to evaluate
  *
- * Return: pointer to the new dog, or NULL if memory allocation fails.
+ * Return: the length of the string
  */
-dog_t *new_dog(char *name, float age, char *owner)
+int _strlen(char *s)
 {
-    dog_t *new_dog;
-    char *name_copy, *owner_copy;
-    int name_length, owner_length;
+	int i;
 
-    /* Calculate the lengths of the name and owner strings */
-    name_length = strlen(name);
-    owner_length = strlen(owner);
+	i = 0;
 
-    /* Allocate memory for the new dog struct */
-    new_dog = malloc(sizeof(dog_t));
-    if (new_dog == NULL)
-        return (NULL);
+	while (s[i] != '\0')
+	{
+		i++;
+	}
 
-    /* Allocate memory for the name and owner strings */
-    name_copy = malloc((name_length + 1) * sizeof(char));
-    if (name_copy == NULL)
-    {
-        free(new_dog);
-        return (NULL);
-    }
-
-    owner_copy = malloc((owner_length + 1) * sizeof(char));
-    if (owner_copy == NULL)
-    {
-        free(name_copy);
-        free(new_dog);
-        return (NULL);
-    }
-
-    /* Copy the name and owner strings */
-    strcpy(name_copy, name);
-    strcpy(owner_copy, owner);
-
-    /* Set the new dog's name, age, and owner */
-    new_dog->name = name_copy;
-    new_dog->age = age;
-    new_dog->owner = owner_copy;
-
-    return (new_dog);
+	return (i);
 }
